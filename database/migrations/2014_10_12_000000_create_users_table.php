ed3a3fb89e733a9f74e92d48dfb5ae426bf04e61
eylay <ali.seyfi68@gmail.com>
2021-10-05 08:45:43 +0330

    necessary migrations ready

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,11 +17,24 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        /*
+        میتونیم به جای اینکه تو این قسمت اضافه کردن کاربر رو انجام بدیم از
+        seeder یا factory
+        هم استفاده کنیم
+        اما از اونجایی که پروژه کوچیک هستش و ما فقط به 1 کاربر برای شروع نیاز داریم ترجیح دادم همینجا 1 کاربر تعریف کنم
+        */
+        \DB::table('users')->insert([
+            'name' => 'elanza',
+            'email' => 'test@elanza.com',
+            'password' => bcrypt('elanza'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
